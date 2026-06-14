0ae41b3b2cba84be34c6c9f21f252ab8f152eaec
DV2145 <buy030117@gmail.com>
2026-06-14 23:38:53 +0900

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://mjwcbyzlzyjeerrtjmxr.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_xlA4vh37aX_71ZsFscY4vA_GGZR8SkS";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
