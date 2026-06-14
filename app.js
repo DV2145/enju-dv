9222531f3c7f88d146bc31f3bd52d45ce845481e
DV2145 <buy030117@gmail.com>
2026-06-15 00:09:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,31 @@ async function getCurrentUser() {
 function formatNumber(num) {
     return Number(num).toLocaleString();
 }
+async function loadProducts() {
+
+    const { data, error } = await supabaseClient
+        .from("products")
+        .select("*")
+        .eq("is_active", true)
+        .order("price");
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    const productsDiv = document.getElementById("products");
+
+    productsDiv.innerHTML = "";
+
+    data.forEach(product => {
+
+        productsDiv.innerHTML += `
+            <button>
+                ${product.name}
+                (${Number(product.price).toLocaleString()}P)
+            </button>
+            <br><br>
+        `;
+    });
+}
