66525ccdc9820263951452872ec7c69a3dee2ea8
DV2145 <buy030117@gmail.com>
2026-06-14 23:59:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+async function getCurrentUser() {
+    const {
+        data: { user }
+    } = await supabaseClient.auth.getUser();
 
+    if (!user) {
+        location.href = "login.html";
+        return null;
+    }
+
+    return user;
+}
+
+function formatNumber(num) {
+    return Number(num).toLocaleString();
+}
